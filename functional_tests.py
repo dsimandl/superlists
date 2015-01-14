c546eb9c14a93bdd667732393399a77dc92222d7
David Simandl <david.simandl@gmail.com>
2015-01-14 17:20:18 -0500

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -25,14 +25,14 @@ class NewVisitorTest(unittest.TestCase):
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         # She types "Buy peacock feathers" into a text box
-        inputbox.send_keys('Bu peacock feathers')
+        inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the page lists
         #1: Buy peacock feathers as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table")
 
         # There is still a text box iviting her to add another item.  She enters "USe peacock feather to make a fly"
         self.fail('Finish the test!')
